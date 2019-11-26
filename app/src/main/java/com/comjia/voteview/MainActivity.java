cff66f3b3f2f0276806e0a897eb6828e6c3ea98f
zhouxuming <zhouxuming@rongcloud.cn>
2019-11-26 17:50:51 +0800

    init project

---
+++ app/src/main/java/com/comjia/voteview/MainActivity.java
@@ -0,0 +1,11 @@
+package com.comjia.voteview;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
